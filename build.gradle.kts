5dfd27dd37ec331ffb206a485e0aa31cae99bc62
lulu2002 <lose9749@gmail.com>
2021-09-07 22:37:26 +0800

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,86 @@
+plugins {
+    kotlin("jvm") version "1.3.71"
+    id("net.minecrell.plugin-yml.bukkit") version "0.3.0"
+    id("com.github.johnrengelman.shadow") version "5.2.0"
+    kotlin("plugin.serialization") version "1.3.71"
+}
+
+group = "me.lulu"
+version = "0.1.0"
+
+val spigot_version = "1.8.8-R0.1-SNAPSHOT"
+val kotlinbukkitapi_version = "0.1.0-SNAPSHOT"
+
+//val hikaricp_version = "3.4.1" // optional dependency
+//val exposed_version = "0.21.1" // optional dependency
+
+repositories {
+    jcenter()
+
+    // Spigot
+    maven { url = uri("https://hub.spigotmc.org/nexus/content/repositories/snapshots/") }
+    maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots/") }
+
+    // KotlinBukkitAPI
+    maven { url = uri("http://nexus.devsrsouza.com.br/repository/maven-public/") }
+
+    maven {
+        name = "citizens-repo"
+        url = uri("https://repo.citizensnpcs.co/")
+    }
+
+    // Backup KotlinBukkitAPI repoistory, in case the above one is not working
+    // maven("https://oss.jfrog.org/oss-snapshot-local/")
+    // maven("http://nexus.okkero.com/repository/maven-releases/") //skedule
+    // maven("https://repo.codemc.org/repository/maven-public") //bstats
+}
+
+dependencies {
+    val changing = Action<ExternalModuleDependency> { isChanging = true }
+
+    compileOnly(kotlin("stdlib-jdk8")) // embedded in KotlinBukkitAPI
+
+    compileOnly("org.spigotmc:spigot-api:$spigot_version")
+
+    // KotlinBukkitAPI
+    compileOnly("br.com.devsrsouza.kotlinbukkitapi:core:$kotlinbukkitapi_version", changing)
+    compileOnly("br.com.devsrsouza.kotlinbukkitapi:architecture:$kotlinbukkitapi_version", changing)
+    compileOnly("br.com.devsrsouza.kotlinbukkitapi:serialization:$kotlinbukkitapi_version", changing)
+    //compileOnly("br.com.devsrsouza.kotlinbukkitapi:exposed:$kotlinbukkitapi_version", changing)
+    //compileOnly("br.com.devsrsouza.kotlinbukkitapi:plugins:$kotlinbukkitapi_version", changing)
+
+    // OPTIONAL EXTRA DEPENDENCIES
+
+    // hikaricp
+    //implementation("com.zaxxer:HikariCP:$hikaricp_version")
+
+    // exposed
+    //implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
+    //implementation("org.jetbrains.exposed:exposed-java-jdbc:$exposed_version")
+    //implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
+}
+
+bukkit {
+    main = "${project.group}.${project.name.toLowerCase()}.${project.name}Plugin"
+    author = "Your Name"
+    website = "yourwebsite.com"
+    depend = listOf("KotlinBukkitAPI")
+
+    description = "The plugin description."
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    shadowJar {
+        classifier = null
+    }
+}
+
+configurations.all {
+    resolutionStrategy.cacheChangingModulesFor(120, "seconds")
+}
---
+++ src/main/kotlin/com/yourname/projectname/Alone.kt
@@ -0,0 +1,15 @@
+package com.yourname.projectname
+
+import br.com.devsrsouza.kotlinbukkitapi.architecture.KotlinPlugin
+import br.com.devsrsouza.kotlinbukkitapi.dsl.command.arguments.player
+import br.com.devsrsouza.kotlinbukkitapi.dsl.command.command
+import br.com.devsrsouza.kotlinbukkitapi.dsl.command.sendSubCommandsList
+import br.com.devsrsouza.kotlinbukkitapi.dsl.command.simpleCommand
+import br.com.devsrsouza.kotlinbukkitapi.extensions.text.msg
+import br.com.devsrsouza.kotlinbukkitapi.extensions.text.plus
+import br.com.devsrsouza.kotlinbukkitapi.extensions.text.textOf
+import org.bukkit.ChatColor
+
+class Alone : KotlinPlugin() {
+    
+}
